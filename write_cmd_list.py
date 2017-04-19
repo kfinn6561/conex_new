0e2ed044919d393a3181d94356897996509a44f0
Kieran Finn <ktf243@nyu.edu>
2017-04-18 23:12:24 -0400

    minor changes

--- write_cmd_list.py
+++ write_cmd_list.py
@@ -1,18 +1,19 @@
 import numpy as np
 
 N0=0.03
-Mss=[0,1,5,7,10,12,15]
-Es=np.linspace(18,20,11)
+Mss=[0,5,7,10,12]
+Es=np.linspace(17.8,20,12)
 #Edict={18:10,18.3:10,18.7:10,19:10,19.3:5,19.7:1,20:1}
 #Es=Edict.keys()
-nsamples=50
+nsamples=20
+persim=10
 outfile='cmd_list.txt'
 f=open(outfile,'w')
 
 
 for Ms in Mss:
     for E in Es:
-        cmd='bin/conex2r -E %g -e %g' %(E,E)
+        cmd='bin/conex2r -E %g -e %g -n %d' %(E,E,persim)
         if Ms==0:
             cmd +=' -Q -x no_cl '
         else:
