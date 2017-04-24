2ee7b7782b51e9c204eed4b1bba9b57791368750
kfinn6561 <ktf43@nyu.edu>
2017-04-24 14:30:17 -0400

    finished changes to write_cmd_list

--- write_cmd_list.py
+++ write_cmd_list.py
@@ -8,23 +8,28 @@ Fs=[0.9,0.5,0.1]
 
 Es=np.linspace(17.8,20,12)
 nsamples=50
+percmd= 1
 outfile='cmd_list.txt'
 f=open(outfile,'w')
 
 if calc_no_cl:
+    for E in Es:
+        cmd='bin/conex2r -n %d -E %g -e %g -Q -x no_cl ' %(percmd,E,E)
+        print cmd
+        for i in range(nsamples):
+            f.write(cmd+'\n')
     
 
 
 for Ms in Mss:
-    for E in Es:
-        cmd='bin/conex2r -E %g -e %g' %(E,E)
-        if Ms==0:
-            cmd +=' -Q -x no_cl '
-        else:
-            cmd += ' -C %d -N %g -c 0.17 -x cl_%d '%(Ms,N0,Ms)
-        print cmd
-        for i in range(nsamples):
-            f.write(cmd+'\n')
+    for Mc in Mcs:
+        for F in Fs:
+            for E in Es:
+                cmd='bin/conex2r -E %g -e %g -n %d' %(E,E,percmd)
+                cmd += ' -C %d -N %g -c %g -f %g -x cl_%d_%03d_%02d '%(Ms,N0,Mc,F,Ms,int(1000*Mc),int(100*F))
+                print cmd
+                for i in range(nsamples):
+                    f.write(cmd+'\n')
 
 f.close() 
 print 'done'
